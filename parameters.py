f53fc5ff56293dc61e9a9db7df44bcde801b9b59
martin.tran <martin.tran@uni-konstanz.de>
2024-04-20 13:04:13 +0200

    add parameters.py

---
+++ parameters.py
@@ -0,0 +1,62 @@
+{
+    "type": "function",
+    "function": {
+        "name": "create_database_entry",
+        "description": "Create a new entry in the database with the given parameters.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "Region of Incident": {"type": "string", "description": ""},
+                "Incident Date": {
+                    "type": "string",
+                    "description": "estimated date of death",
+                },
+                "Number of Dead": {
+                    "type": "string",
+                    "description": "total number of dead people",
+                },
+                "Number of Missing": {
+                    "type": "string",
+                    "description": "total number of those who are missing and are thus assumed to be dead",
+                },
+                "Number of Survivors": {
+                    "type": "string",
+                    "description": "number of migrants that survived the incident",
+                },
+                "Country of Origin": {
+                    "type": "string",
+                    "description": "Country of birth of the decedent",
+                },
+                "Region of Origin": {
+                    "type": "string",
+                    "description": "Region of origin of the decedent(s)",
+                },
+                "Cause of Death": {
+                    "type": "string",
+                    "description": "determination of conditions resulting in the migrant's death i.e. the circumstances of the event that produced the fatal injury",
+                },
+                "Country of Incident": {
+                    "type": "string",
+                    "description": "country where incident occured",
+                },
+                "Location of Incident": {
+                    "type": "string",
+                    "description": "Place where the death(s) occurred or where the body or bodies were found. Nearby towns or cities or borders are included where possible. When incidents are reported in an unspecified location, this will be noted",
+                    "enum": "longitude and latitude",
+                },
+            },
+            "required": [
+                "Region of Incident",
+                "Incident Date",
+                "Number of Dead",
+                "Number of Missing",
+                "Number of Survivors",
+                "Country of Origin",
+                "Region of Origin",
+                "Cause of Death",
+                "Country of Incident",
+                "Location of Incident",
+            ],
+        },
+    },
+}
